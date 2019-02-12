cf3cd6e397020c517cc5bcb8e426f8bc7da22569
IQbrod <iqbrod@outlook.fr>
2019-02-12 12:02:26 +0100

    Initial Commit : Q1

---
+++ Explosives.java
@@ -0,0 +1,69 @@
+// Based on a B specification from Marie-Laure Potet.
+
+public class Explosives{
+    public int nb_inc = 0;
+    public String [][] incomp = new String[50][2];
+    public int nb_assign = 0;
+    public String [][] assign = new String[30][2];
+ 
+    // -- P1: On ne peut pas avoir plus de 25 paires de produits incompatibles (et on ne peut pas avoir un nombre négatif) --
+    /*@ public invariant
+    @ (0 <= nb_inc && nb_inc < 50);
+    @*/
+    
+    // -- P2: On ne peut pas ajouter plus de 30 produits dans les batiments (et on ne peut pas avoir un nombre négatif) --
+    /*@ public invariant 
+    @ (0 <= nb_assign && nb_assign < 30);
+    @*/
+
+    // -- P3: Les incompatibilités doivent concerner deux produits --
+    /*@ public invariant
+    @ (\forall int i; 0 <= i && i < nb_inc; 
+    @         incomp[i][0].startsWith("Prod") && incomp[i][1].startsWith("Prod"));
+    @*/
+
+    // -- P4: Les assignations concerne d'abord un batiment puis un produit --
+    /*@ public invariant 
+    @ (\forall int i; 0 <= i && i < nb_assign; 
+    @         assign[i][0].startsWith("Bat") && assign[i][1].startsWith("Prod"));
+    @*/
+
+    // -- P5: L'incompatibilité doit concerner deux produits différents --
+    /*@ public invariant 
+    @ (\forall int i; 0 <= i && i < nb_inc; !(incomp[i][0]).equals(incomp[i][1]));
+    @*/
+
+    // -- P6: Les incompatibilités mirroirs doivent exister (I: A /!\ B && J: B /!\ A) --
+    /*@ public invariant 
+    @ (\forall int i; 0 <= i && i < nb_inc; 
+    @        (\exists int j; 0 <= j && j < nb_inc; 
+    @           (incomp[i][0]).equals(incomp[j][1]) 
+    @              && (incomp[j][0]).equals(incomp[i][1]))); 
+    @*/
+
+    // -- P7: Pour toutes les assignations, les produits incompatibles à un produit présent dans le batiment ne doivent pas être présents dans ce batiment (Si au moins 2 produits sont présents dans le batiment) --
+    /*@ public invariant 
+    @ (\forall int i; 0 <= i &&  i < nb_assign; 
+    @     (\forall int j; 0 <= j && j < nb_assign; 
+    @        (i != j && (assign[i][0]).equals(assign [j][0])) ==>
+    @        (\forall int k; 0 <= k && k < nb_inc;
+    @           (!(assign[i][1]).equals(incomp[k][0])) 
+    @              || (!(assign[j][1]).equals(incomp[k][1])))));
+    @*/
+
+
+    public void add_incomp(String prod1, String prod2){
+      incomp[nb_inc][0] = prod1;
+      incomp[nb_inc][1] = prod2;
+      incomp[nb_inc+1][1] = prod1;
+      incomp[nb_inc+1][0] = prod2;
+      nb_inc = nb_inc+2;
+     }
+    public void add_assign(String bat, String prod){
+      assign[nb_assign][0] = bat;
+      assign[nb_assign][1] = prod;
+      nb_assign = nb_assign+1;
+    }
+    public void skip(){
+    }
+}
---
+++ TestExplosivesJUnit4.java
@@ -0,0 +1,130 @@
+import static org.junit.Assert.*;
+
+import org.jmlspecs.utils.JmlAssertionError;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class TestExplosivesJUnit4 {
+
+    static int nb_inconclusive = 0;
+    static int nb_fail = 0;
+
+    Explosives e;
+
+    public static void main(String args[]) {
+    	String testClass = "TestExplosivesJUnit4";
+     	org.junit.runner.JUnitCore.main(testClass);
+     }
+
+
+    private void handleJMLAssertionError(JmlAssertionError e) {
+    	if (e.getClass().equals(JmlAssertionError.PreconditionEntry.class)) {
+    	    System.out.println("\n INCONCLUSIVE "+(new Exception().getStackTrace()[1].getMethodName())+ "\n\t "+ e.getMessage());
+            nb_inconclusive++;}
+    else{
+	    // test failure	
+	    nb_fail++;
+	    fail("\n\t" + e.getMessage());	
+		}  
+    }
+	
+    
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		nb_inconclusive = 0;
+		nb_fail = 0;
+		org.jmlspecs.utils.Utils.useExceptions = true;
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	     System.out.println("\n inconclusive tests: "+nb_inconclusive+" -- failures : "+nb_fail );
+	}
+	
+	/*@Test
+	// Test déjà fourni par le sujet
+	public void  test_P7() {
+		try{
+			e=new Explosives();
+			e.add_incomp("Prod_Nitro","Prod_Glycerine");
+			e.add_incomp("Prod_Dyna","Prod_Mite");
+			e.add_assign("Bat_1","Prod_Dyna");
+			e.add_assign("Bat_1","Prod_Nitro");
+			e.add_assign("Bat_2","Prod_Mite");
+			e.add_assign("Bat_1","Prod_Glycerine");
+		} 	catch(JmlAssertionError e){
+				handleJMLAssertionError(e);		
+		}  
+	}*/
+
+	/*@Test
+	public void test_P1() {
+		try{
+			e=new Explosives();
+			int i = 0;
+			while (i < 26) { // >25
+				e.add_incomp("Prod_"+i,"Prod_100");
+				i++;
+			}			
+		} 	catch(JmlAssertionError e){
+				handleJMLAssertionError(e);		
+		}
+	}*/
+
+	/*@Test
+	public void test_P2() {
+		try{
+			e=new Explosives();
+			int i = 0;
+			while (i < 31) { // >30
+				e.add_assign("Bat_1","Prod_1");
+				i++;
+			}			
+		} 	catch(JmlAssertionError e){
+				handleJMLAssertionError(e);		
+		}
+	}*/
+
+	/*@Test 
+	public void test_P3() {
+		try{
+			e=new Explosives();
+			//e.add_incomp("SomethingWrong","Prod_X");	
+			e.add_incomp("Prod_X","SomethingWrong");
+			//e.add_incomp("SomethingWrong","SomethingElse");		
+		} 	catch(JmlAssertionError e){
+				handleJMLAssertionError(e);		
+		}
+	}*/
+
+	/*@Test
+	public void test_P4() {
+		try{
+			e=new Explosives();	
+			//e.add_assign("Bat_X","SomethingWrong");
+			//e.add_assign("SomethingWrong","Prod_X");
+			e.add_assign("Prod_X","Bat_Y");	
+		} 	catch(JmlAssertionError e){
+				handleJMLAssertionError(e);		
+		}
+	}*/
+
+	/*@Test
+	public void test_P5() {
+		try{
+			e=new Explosives();	
+			e.add_incomp("Prod_X","Prod_X");	
+		} 	catch(JmlAssertionError e){
+				handleJMLAssertionError(e);		
+		}
+	}*/
+
+	@Test
+	public void test_P6() {
+		/* Impossible sans modifier les éléments "public" */
+	}
+
+
+}
